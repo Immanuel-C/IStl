c47bd7004262947c28c116d47bb300704a589638
Immanuel Charles <73501113+Immanuel-C@users.noreply.github.com>
2024-02-13 19:46:32 -0500

    removed compiler specific defines

--- src/_IStlCore.hpp
+++ src/_IStlCore.hpp
@@ -4,24 +4,14 @@
 
 #if ISTL_CXX_VERSION >= ISTL_CXX17
 	#define ISTL_NODISCARD [[nodiscard]]
+#else // ISTL_CXX_VERSION >= ISTL_CXX17 
+	#define ISTL_NODISCARD 
 #endif // ISTL_CXX_VERSION >= ISTL_CXX17
 
 #if ISTL_CXX_VERSION >= ISTL_CXX11
 	#define ISTL_NORETURN [[noreturn]] 
 #else // ISTL_CXX_VERSION >= ISTL_CXX11
-	#ifdef __GNUC__
-		#define ISTL_NORETURN __attribute__((noreturn))
-		#define ISTL_NODISCARD __attribute__((warn_unused_result))
-	#elif __MINGW32__
-		#define ISTL_NORETURN __attribute__((noreturn))
-		#define ISTL_NODISCARD __attribute__((warn_unused_result))
-	#elif __clang__
-		#define ISTL_NORETURN __attribute__((noreturn))
-		#define ISTL_NODISCARD __attribute__((warn_unused_result))
-	#elif _MSC_VER
-		#define ISTL_NORETURN __declspec(noreturn)
-		#define ISTL_NODISCARD _Check_return_ 
-	#endif
+	#define ISTL_NORETURN 
 #endif // ISTL_CXX_VERSION >= ISTL_CXX11
 
 #ifdef ISTL_CXX_VERSION >= ISTL_CXX20
